813583614e7d1697b53596503f4744f4cb54f2c0
Reshma Khilnani <reshma.khilnani@gmail.com>
2014-10-07 18:39:38 -0700

    updating problem 9

--- src/test/Problem9.scala
+++ src/test/Problem9.scala
@@ -1,15 +1,14 @@
 package test
 
 object Problem9 extends App {
-  //35541072
+  //31875000
   var a = 1;
   var b = 2;
   var fin = true;
   while (fin) {
     while (a < b) {
-      var c2 = Math.pow(a, 2.0) + Math.pow(b, 2.0);
-      var c = Math.sqrt(c2).toInt;
-      if (a + b + c == 1000) {
+      var c = 1000 - a - b;
+      if (a * a + b * b == c * c) {
         var prod = a * b * c;
         println(prod);
         fin = false;
